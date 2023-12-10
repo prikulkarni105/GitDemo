4c2ecc4d57fc4d9353d40b16309afea410467f79
priya <priya@Priyanka>
2023-12-10 14:49:13 +0530

    mster

---
+++ src/main/java/JobInt/App.java
@@ -0,0 +1,31 @@
+package JobInt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	String name="Priyanka Dhiraj Kulkarni";
+    	
+    	
+    	String names[]=	name.split(" ");
+    	
+    	
+    	System.out.println(names[0]);
+    	
+    	
+    	
+    }
+}
---
+++ src/main/java/JobInt/Interview.java
@@ -0,0 +1,24 @@
+package JobInt;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Interview {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+    System.out.println("Hi, General git practice");
+    
+	}
+
+}
